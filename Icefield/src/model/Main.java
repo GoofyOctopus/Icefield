362dd0919948811dbbe22eca7cb8d14611b96317
GoofyOctopus <yazansuleiman00@hotmail.com>
2020-03-26 18:14:59 +0100

    aaa

--- Icefield/src/model/Main.java
+++ Icefield/src/model/Main.java
@@ -11,11 +11,8 @@ public class Main {
 		System.out.println("Hello world!");
 		System.out.println("Done by the bombe!");
 		System.out.println("khosoo");
-
 		System.out.println("khosoo");
 		System.out.println("khosoo");
 		System.out.println("khosoo");
-	
-
 	}
 }
