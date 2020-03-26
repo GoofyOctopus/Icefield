ab1536d2f38ba25ead689faf030455461f415c78
Erdenechuluun Khuderchuluun <tesoro.ec@gmail.com>
2020-03-26 17:47:10 +0100

    5 th commit

--- Icefield/src/model/Main.java
+++ Icefield/src/model/Main.java
@@ -5,9 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Yazan");
+		System.out.println("Erdene");
 		System.out.println("Edited by smn else!");		
 		System.out.println("Hello world!");
 		System.out.println("Done by the bombe!");
+		
 	}
 
 }
