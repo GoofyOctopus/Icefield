d5fa3bb2d8b1a8937f529958df251d7bb3016405
Zurab Tsinadze <zurab.tsinadze@ericsson.com>
2020-03-26 19:40:56 +0100

    Reading number of players in startGame()

--- Icefield/src/model/Game.java
+++ Icefield/src/model/Game.java
@@ -1,4 +1,5 @@
 package model;
+import java.util.Scanner;
 
 import Figures.Figure;
 
@@ -7,11 +8,18 @@ public class Game {
     public int roundCounter;
     private int numberOfMoves;
 
-    public void startGame(){}
-    public void chooseFigure(){}
+    public void startGame(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of players:");
+        numberOfFigures = in. nextInt();
+        roundCounter = 0;
+        numberOfMoves = 0;
+    }
+    public void chooseFigure(int numberOfFigures){}
     public void checkFlareGun(){}
     public void winGame(){}
     public void makeMove(Figure currPl){}
     public void nextPlayer(){}
     public void endGame(){}
+
 }
