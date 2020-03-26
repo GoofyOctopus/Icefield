49a4021fe7d2c9ac2e0cfa455f49de4566c2e479
GoofyOctopus <yazansuleiman00@hotmail.com>
2020-03-26 18:05:23 +0100

    8th

--- Icefield/src/model/Main.java
+++ Icefield/src/model/Main.java
@@ -6,9 +6,11 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Yazan");
 		System.out.println("Erdene");
+		System.out.println("kamyar");
 		System.out.println("Edited by smn else!");		
 		System.out.println("Hello world!");
 		System.out.println("Done by the bombe!");
+		System.out.println("khosoo");
 		
 	}
 }
