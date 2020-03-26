4d1ca16dd614055f8ee72fe13131d7a26df39c0b
khoso <khoso@192.168.0.248>
2020-03-26 18:45:48 +0100

    khosoo 1st

---
+++ Icefield/src/Figures/Figure.java
@@ -0,0 +1,33 @@
+package Figures;
+
+import model.Dircetion;
+
+public abstract class Figure {
+	boolean isDrowning;
+	int roundOfDrowning;
+	boolean isWearingDivingSuit;
+	
+	void step(Dircetion d) {
+		
+	}
+	
+	void die() {
+		
+	}
+	
+	void decreaseHeatUnit() {
+		
+	}
+	
+	void increaseHeatUnit() {
+		
+	}
+	
+	void removeSnow() {
+		
+	}
+	
+	void retrieveItem(Item i) {
+		
+	}
+}
