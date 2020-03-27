8feb44a33b5d121a8b287d86e7c05c550091ee30
khoso <khoso@192.168.0.248>
2020-03-27 18:24:59 +0100

    khosoo 1st

--- Icefield/src/Figures/Figure.java
+++ Icefield/src/Figures/Figure.java
@@ -1,15 +1,30 @@
 package Figures;
 
+import java.util.ArrayList;
+
 import Item.IItem;
 import model.Direction;
+import model.Iceberg;
 
 public abstract class Figure {
 	boolean isDrowning;
 	int roundOfDrowning;
+	int bodyHeatUnit;
 	boolean isWearingDivingSuit;
+	Iceberg iceberg;
+	Iceberg neighboringIceberg;
+	ArrayList<IItem> inventory;
 	
+	public Figure(Iceberg iceberg) {
+		this.isDrowning = false;
+		this.roundOfDrowning = 0;
+		this.isWearingDivingSuit = false;
+		this.iceberg = iceberg;
+		this.inventory = new ArrayList<IItem>();
+	}
 	void step(Direction d) {
-		
+		iceberg.remove(this);
+		iceberg.getNeighbor(d).accept(this);
 	}
 	
 	void die() {
@@ -17,22 +32,29 @@ public abstract class Figure {
 	}
 	
 	void decreaseHeatUnit() {
-		
+		bodyHeatUnit--;
 	}
 	
 	void increaseHeatUnit() {
-		
+		bodyHeatUnit++;
 	}
 	
 	void removeSnow() {
-		
+		iceberg.decreaseSnow(1);
 	}
 	
 	void retrieveItem(IItem i) {
 		
 	}
 	
-	void useSkill() {
-		
+	void help(Iceberg i1) {
+		iceberg.remove(this);
+		i1.accept(this);
 	}
+	
+	void drown() {
+		isDrowning = true;
+	}
+	
+	abstract int useSkill(Direction d);
 }
--- Icefield/src/Figures/PolarExplorer.java
+++ Icefield/src/Figures/PolarExplorer.java
@@ -1,5 +1,19 @@
 package Figures;
 
+import model.Direction;
+import model.Iceberg;
+
 public class PolarExplorer extends Figure{
+
+	public PolarExplorer(Iceberg iceberg) {
+		super(iceberg);
+		bodyHeatUnit = 4;
+	}
+
+	@Override
+	int useSkill(Direction d) {
+		return 0; //////////////////////////////
+	}
+	
 
 }
