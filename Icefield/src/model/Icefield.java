cfc5e2c5a9ee6014bde3676c4aaa63e53127ce35
GoofyOctopus <yazansuleiman00@hotmail.com>
2020-03-28 19:02:29 +0100

    adding neighbors

--- Icefield/src/model/Icefield.java
+++ Icefield/src/model/Icefield.java
@@ -1,22 +1,25 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
 
-//import model.StableIceberg;
-//import model.UnstableIceberg;
+import Item.IItem;
+
 /*
  * Yazan
  */
 public class Icefield {
-	private Iceberg icebergs[][];
+	private Iceberg icebergs[][];;
+	
 	/*
-	 * Default constructor initialize the attribute we have
-	 * and instantiate the iceberg object according to a 
+	 * Default constructor initializes the icebergs 2d array
+	 * and instantiate the iceberg objects according to a 
 	 * specific algorithm, where all the sides are unstable
 	 * and some few icebergs are unstable according to a
 	 * relation between the counters i&j, hence, this might
 	 * change if the future to increase the difficulty of
-	 * the game
+	 * the game. Also in the constructor the icebergs'
+	 * neighbors are set.
 	 */
 	public Icefield()
 	{ 
@@ -30,8 +33,19 @@ public class Icefield {
 					icebergs[i][j]= new UnstableIceberg(i,j);
 				else
 					icebergs[i][j]= new StableIceberg(i,j);
+				if(j > 0)
+				{
+					icebergs[i][j].setBorderingIceberg(Direction.UP, icebergs[i][j-1]);
+					icebergs[i][j-1].setBorderingIceberg(Direction.DOWN, icebergs[i][j]);
+				}
+				if(i > 0)
+				{
+					icebergs[i][j].setBorderingIceberg(Direction.LEFT, icebergs[i-1][j]);
+					icebergs[i-1][j].setBorderingIceberg(Direction.RIGHT, icebergs[i][j]);
+				}
 			}
 		}
+		
 	}
 	/*
 	 * generateBlizzards() method is used called from the Game class
