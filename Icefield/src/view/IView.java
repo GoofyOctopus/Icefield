2e417362d02eb9aa1ae5a124711bbd00b4c97323
GoofyOctopus <yazansuleiman00@hotmail.com>
2020-05-15 00:25:55 +0200

    Connecting the game frame to the controller

---
+++ Icefield/src/view/IView.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+
+public interface IView {
+	/*
+	 * This method is called from the Controller, every component here
+	 * should be updated to be consistent with the model
+	 */
+	public void update();
+	/*
+	 * The following methods are used when adding the listeners(obviously:))
+	 */
+	public void addBtnListener(ActionListener myListener);
+	public void addKeyListener(KeyListener myListener);
+}
